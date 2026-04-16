ece33c7803e1b4963428ef978d6f4fea375780a1
b4ry0n <git.x7k9m2@b4ry0n.eu>
2026-04-16 21:40:44 +0200

    fix: sort_by_key for case-insensitive sort (clippy)

--- filetag-web/src/api.rs
+++ filetag-web/src/api.rs
@@ -417,8 +417,8 @@ pub async fn api_files(
         }
     }
 
-    dirs.sort_by(|a, b| a.name.to_lowercase().cmp(&b.name.to_lowercase()));
-    files.sort_by(|a, b| a.name.to_lowercase().cmp(&b.name.to_lowercase()));
+    dirs.sort_by_key(|a| a.name.to_lowercase());
+    files.sort_by_key(|a| a.name.to_lowercase());
     dirs.extend(files);
 
     Ok(Json(ApiDirListing {
